63065b60d44582192d598230673c0c1257d8dff6
SeungHwan Baek <qorwhkdid@gmail.com>
2017-06-07 17:46:23 +0900

    주차장 정보, 공지사항 컨트롤, 공지사항 jsp 테스트 승환

--- parkingProject/src/main/java/com/parking/noticeBoard/controller/NoticeBoardController.java
+++ parkingProject/src/main/java/com/parking/noticeBoard/controller/NoticeBoardController.java
@@ -1,8 +1,14 @@
 package com.parking.noticeBoard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/noticeBoard")
 public class NoticeBoardController {
 
+	@RequestMapping(value = "/noticeBoardMain")
+	public String noticeBoardMain() {
+		return "noticeBoard/noticeBoardMain";
+	}
 }
